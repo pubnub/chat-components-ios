f1de611f6b366e8924ffc452a91f72e2cda6277e
Maciej Adamczyk <maciej.adamczyk@pubnub.com>
2022-08-31 19:40:23 +0200

    removed file for provider extension

--- Sources/PubNubChatComponents/Views/Reaction/Provider/ReactionProvider.swift
+++ Sources/PubNubChatComponents/Views/Reaction/Provider/ReactionProvider.swift
@@ -7,8 +7,20 @@
 
 import Foundation
 
+/// Use this protocol to implement own custom ReactionProvider, instead you can use DefaultReactionProvider or CustomReactionProvider
 public protocol ReactionProvider {
+  /// Provides list of available reactions
   var reactions: [String] { get }
+}
+
+extension ReactionProvider {
+  func makeMessageReactionComponents() -> [MessageReactionButtonComponent] {
+    reactions.map {
+      let result = MessageReactionButtonComponent(type: .custom)
+      result.reaction = $0
+      return result
+    }
+  }
 }
 
 public struct DefaultReactionProvider: ReactionProvider {
